dd3a2cd9e620c918fd805830e5a0a71243ca60c5
Mariusz Mazur <mmazur@redhat.com>
2019-09-26 16:41:29 +0200

    Add containerized-data-importer
    Switch from release titles to release tags, duh.

--- update-repo-releases.py
+++ update-repo-releases.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 OUTPUT_DIR='gh-pages'
 GH_ORG='kubevirt'
@@ -29,9 +29,10 @@ g = Github(os.environ['GITHUB_TOKEN'])
 repo = g.get_repo('{0}/{1}'.format(GH_ORG, repo_name))
 
 # TODO: stricter enforcement of version string pattern? To reduce chance of an accident?
-releases = {r.title: r for r in repo.get_releases() if len(r.title)>1 and r.title.startswith('v')}
+releases = {r.tag_name: r for r in repo.get_releases() if r.tag_name and re.match('^v[0-9]+', r.tag_name)}
 
-full_releases = {k: v for k, v in releases.items() if re.match('^v[\.0-9]*$', k)}
+# TODO: what else other than v.draft?
+full_releases = {k: v for k, v in releases.items() if re.match('^v[\.0-9]*$', k) and not v.draft}
 sorted_full_versions = sorted(full_releases.keys(), key=LooseVersion, reverse=True)
 latest = sorted_full_versions[0]
 
